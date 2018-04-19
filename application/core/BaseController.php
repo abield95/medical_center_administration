6277ca3165dab313c4d741ba9ac8dcd31b91bdca
abield95 <santosabield55@gmail.com>
2018-04-19 17:13:02 -0400

    An Update

---
+++ application/core/BaseController.php
@@ -0,0 +1,26 @@
+<?php 
+
+	/**
+	* Base controller
+	*/
+	class BaseController
+	{
+		public $View;
+
+		function __construct()
+		{
+			//always initialize a session
+			Session::init();
+
+			//Auth::checkSessionConcurrency();
+
+			/*if (!Session::userIsLoggedIn() AND Request::cookie('remember_me')) {
+				# code...
+				header('location: ' . Config::get('URL') . 'login/loginWithCookie');
+			}*/
+
+			$this->View = new View();
+		}
+	}
+
+ ?>
